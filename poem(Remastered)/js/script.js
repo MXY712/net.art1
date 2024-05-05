7b4e3032eeab46e7be68b92163dbce87e34f2b54
MXY712 <157234311+MXY712@users.noreply.github.com>
2024-05-05 14:44:55 -0400

    Add files via upload

---
+++ poem(Remastered)/js/script.js
@@ -0,0 +1,26 @@
+function togglePoem() {
+    var poem = document.getElementById('poem');
+    if (poem.style.display === 'none') {
+      poem.style.display = 'block';
+    } else {
+      poem.style.display = 'none';
+    }
+  }
+  
+  function highlightVerse(verse) {
+    if (verse.style.color === 'red') {
+      verse.style.color = '';
+      verse.style.fontWeight = '';
+    } else {
+      verse.style.color = 'red';
+      verse.style.fontWeight = 'bold';
+    }
+  }
+
+  document.getElementById('dynamicImage').addEventListener('click', function() {
+    var currentSrc = this.src;
+    console.log('img/0dba819e360c53e533667e18a5323aa.png', currentSrc); // 打印当前的图片源地址，帮助调试
+    this.src = currentSrc.includes('img/0dba819e360c53e533667e18a5323aa.png') ? 'img/38ebe002c22e11566338459d8df80fa.png' : 'img/0dba819e360c53e533667e18a5323aa.png';
+    console.log('img/38ebe002c22e11566338459d8df80fa.png', this.src); // 打印新的图片源地址，帮助调试
+  });
+  
